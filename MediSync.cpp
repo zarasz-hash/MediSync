8e7f21cfb38569d70c6e5f9aa57c64c6fa6cdecf
Sarasuhail <suhas@farmingdale.edu>
2025-11-24 11:56:49 -0500

    Update MediSync.cpp
    Removed commented-out code and duplicate sections. Streamlined the login function and main menu logic.

--- MediSync.cpp
+++ MediSync.cpp
@@ -1,14 +1,4 @@
 // MediSync.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-
 
 //ROOOMS TO ADD SIMILAR TO HOW WE HAD HALLS AND LABS IN CAMPIX: Emergency Room (ER) Operating Room Patient Room ICU Room Imaging Room (covers X-ray/CT/MRI in general) Laboratory Room Recovery Room
 
@@ -63,3 +53,4 @@ int main() {
 
 
 //ROOOMS TO ADD SIMILAR TO HOW WE HAD HALLS AND LABS IN CAMPIX: Emergency Room (ER) Operating Room Patient Room ICU Room Imaging Room (covers X-ray/CT/MRI in general) Laboratory Room Recovery Room
+
